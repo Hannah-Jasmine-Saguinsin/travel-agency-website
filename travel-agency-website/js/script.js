a84d37543a06302ac0fa794db560621e76e65037
hanniefuzz <hjsaguinsin16@gmail.com>
2026-04-14 18:47:25 +0800

    navbar-update-final
    What changed?
    -Removed bootstrap on the other html files.
    -Fixed file links connectivity.
    -Fixed file readability (HTML).
    -Adding Description on top of the links (eg. Global CSS and JS link, Navbar CSS and JS, and given CSS page of each file)
    -Removed parts surnames.
    -Added navbar folder with the CSS and JS.
    -Fully functional and interactive navbar.

--- travel-agency-website/js/script.js
+++ travel-agency-website/js/script.js
@@ -1,10 +1,3 @@
-const toggle = document.querySelector('.menu-toggle');
-    const nav = document.querySelector('.nav-links');
-
-    toggle.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
-
 const destinations = [
     "Paris, France",
     "Tokyo, Japan",
